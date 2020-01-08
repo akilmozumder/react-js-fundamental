8d60df019c852021e77018ba04ed8a5ad48e5ba5
Imtiaz Akil <akilfpi@gmail.com>
2020-01-08 23:57:19 +0600

    First commit

---
+++ my-app/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+      /* JSX= javascript xml, this is look like html but its not html its convert html to js;
+      function component: using function and method
+      class component: using class for making component
+      */
+    <div className="App">
+      <h1>Imtiaz Bin Gias</h1>
+      {/* react can use javascript expression */}
+      <h3>{3+4+5}</h3>
+    </div>
+  );
+}
+
+export default App;
